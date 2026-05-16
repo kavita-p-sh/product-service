f7e10d3073c44d65bdfa10658867075bb836e380
Kavita Prajapati <prajapatikavita132004@gmail.com>
2026-05-16 17:03:35 +0530

    Fix product service Maven dependencies and package scanning

--- src/main/java/com/ecommerce/product/ProductServiceApplication.java
+++ src/main/java/com/ecommerce/product/ProductServiceApplication.java
@@ -2,22 +2,16 @@ package com.ecommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {
 		"com.ecommerce.product",
-		"com.ecommerce.common.exception",
-		"com.ecommerce.common.dto",
-		"com.ecommerce.common.util",
-		"com.ecommerce.common.audit"
+		"com.ecommerce.common"
 })
 @EntityScan(basePackages = {
 		"com.ecommerce.common.entity"
 })
-@EnableJpaRepositories(basePackages = {
-		"com.ecommerce.common.repository"
-})
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
